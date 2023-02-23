8f69debd3e57d787f757795d58266c36475b817d
amruta-07 <misalamruta08@gmail.com>
2023-02-23 17:31:16 +0530

    Landing page

---
+++ assets/js/custom.js
@@ -0,0 +1,52 @@
+(function ($) {
+	
+	
+
+
+
+
+	$('.owl-service-item').owlCarousel({
+		items:3,
+		loop:true,
+		dots: true,
+		nav: true,
+		autoplay: true,
+		margin:30,
+		  responsive:{
+			  0:{
+				  items:1
+			  },
+			  600:{
+				  items:2
+			  },
+			  1000:{
+				  items:3
+			  }
+		  }
+	  })
+
+	$('.owl-courses-item').owlCarousel({
+		items:4,
+		loop:true,
+		dots: true,
+		nav: true,
+		autoplay: true,
+		margin:30,
+		  responsive:{
+			  0:{
+				  items:1
+			  },
+			  600:{
+				  items:2
+			  },
+			  1000:{
+				  items:4
+			  }
+		  }
+	  })
+	
+
+	
+
+
+})(window.jQuery);
